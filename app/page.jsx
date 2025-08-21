2217547b8b7b3edd754c9ac9048b5be5a6c25994
Soumwadeep Guha <soumwadeepguha@Soumwadeeps-Mac-mini.local>
2025-08-21 20:29:47 +0530

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the National Remote School</h1>
+      <p>Your platform for remote learning</p>
+    </div>
+  )
+}
+
+export default Home
