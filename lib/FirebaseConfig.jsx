9f73d43bd12995b28d05bbe1e6bd51cadee7dd57
Soumwadeep Guha <soumwadeep@gmail.com>
2025-08-22 11:26:59 +0530

    Fixed Analytics Bug

--- lib/FirebaseConfig.jsx
+++ lib/FirebaseConfig.jsx
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,7 +13,21 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Auth & Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Analytics (only if supported & in browser)
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((yes) => {
+    if (yes) {
+      analytics = getAnalytics(app);
+    }
+  });
+}
+
+export { analytics };
