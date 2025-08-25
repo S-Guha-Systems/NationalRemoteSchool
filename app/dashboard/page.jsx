56cb71fa6975c265c8825a99ebd411db7383cf15
Soumwadeep Guha <soumwadeep@gmail.com>
2025-08-25 20:40:01 +0530

    Added gdrive folder

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,6 @@
 import LeftAlignedContentModel from "@/components/LeftAlignedContentModel";
 import dashboard from "@/img/dashboard.webp";
-import { fetchSingleBook } from "@/lib/Backend";
-import { Box, Typography, Card, CardContent, Grid } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
 
 export const metadata = {
   title: "Dashboard - National Remote School",
@@ -9,16 +8,6 @@ export const metadata = {
 };
 
 export default async function Dashboard() {
-  const { data, error } = await fetchSingleBook("ENGLISH");
-
-  if (error) {
-    return (
-      <Box p={3}>
-        <Typography color="error">Error: {error}</Typography>
-      </Box>
-    );
-  }
-
   return (
     <Box>
       <LeftAlignedContentModel
@@ -29,29 +18,12 @@ export default async function Dashboard() {
       <Typography variant="h4" align="center" gutterBottom>
         Class 1 Books
       </Typography>
-
-      <Grid container spacing={3}>
-        {data.map((book, index) => (
-          <Grid size={{ xs: 12, md: 6, lg: 4 }} key={index}>
-            <Card
-              key={index}
-              sx={{ p: 2, boxShadow: 3, borderRadius: 2, overflow: "hidden" }}
-            >
-              <CardContent>
-                <Typography variant="h6" textAlign="center" gutterBottom>
-                  {book.name}
-                </Typography>
-                <Box
-                  component="iframe"
-                  src={book.url}
-                  width="100%"
-                  height="500px"
-                  sx={{ border: "1px solid #ccc", borderRadius: 1 }}
-                />
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+      {/* https://drive.google.com/embeddedfolderview?id=FOLDER-ID#grid */}
+      <Grid container spacing={3} justifyContent="center" padding={2}>
+        <iframe
+          src="https://drive.google.com/embeddedfolderview?id=1aDT6tjTuSphPogXWSyetr7kRe9NVbQpQ#grid"
+          className="iframecss"
+        ></iframe>
       </Grid>
     </Box>
   );
